5dbd6b62d47d4f69e8c280256116fa5028b6eb4a
Obito <obitodih@gmail.com>
2025-05-18 01:33:33 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,25 @@
 // This is the First Project in Rust
-// This code made by for GREPING OUT
+// This code made For A nice Cli Tool
 // if you are a baddy dm me @x9639 in telegram
 // Enjoy i guess
 // Importing Env from Standerd library
-use std::{env, fs};
+use std::env;
 // Importing Some Color
 use colored::{Color,Colorize};
-mod Ai;
-fn Greper(args: &Vec<String>) {
-
-   // If The lovley user dont provide the needed Arguments
-
-   if args.len() < 4 {
-
-       eprintln!("Usage: baddyCli <fileName> <Word>\nLove U.");
-       std::process::exit(1);
-   }
-   // Get Content Off The File
-   let Content = fs::read_to_string(&args[2]).expect("FuckOff");
-   // For loop for Each Line
-   for (lineNum,line) in Content.lines().enumerate() {
-       // Seeing if the word of yours in the Line ( btw you look cute )
-       if line.contains(&args[3]) {
-           // Do you support Femboys ?
-           println!("[{}] : {}",lineNum,line.replace(&args[3],&args[3].color(Color::BrightCyan).to_string()));
-       }
-   }
-   // Printing Cute msg
-   println!("Done. Love you babe");
-}
-fn HelpFunc(){
-    println!("Usage: baddyCli <mode>\n\nGreper:\nbaddyCli -grep <fileName> <word>\nAi:\nbaddyCli -ai <prompt>\nHelp:\nbaddyCli -h\nLove U.");
-}
+mod utils;
+use utils::{AiMod,Greper,HelpFunc,Infor};
 enum ParseModes {
     AiMode,
     HelpMode,
     GreperMode,
+    InfoMode,
 }
 fn ParseMode(input: &str) -> ParseModes {
     match input {
         "-ai" => ParseModes::AiMode,
         "-h" => ParseModes::HelpMode,
         "-grep" => ParseModes::GreperMode,
+        "-info" => ParseModes::InfoMode,
         _ => ParseModes::HelpMode,
     }
 }
@@ -50,10 +28,10 @@ fn main() {
    let args: Vec<String> = env::args().collect();
    if args.len() < 2 {
 
-       eprintln!("Usage: baddyCli <mode>\n\nGreper:\nbaddyCli -grep <fileName> <word>\nAi:\nbaddyCli -ai <prompt>\nHelp:\nbaddyCli -h\nLove U.");
+       eprintln!("Usage: baddyCli <mode>\n\nGreper:\nbaddyCli -grep <fileName> <word>\nAi:\nbaddyCli -ai <prompt>\nInfo About device :\nbaddyCli -info\nHelp:\nbaddyCli -h\nLove U.");
        std::process::exit(1);
    }
-   let TypeOf = ParseMode(&args[1]);
+   let TypeOf: ParseModes = ParseMode(&args[1]);
    match TypeOf {
        ParseModes::HelpMode => {
            HelpFunc();
@@ -62,8 +40,12 @@ fn main() {
            Greper(&args);
        },
        ParseModes::AiMode => {
-           Ai::AiMod(&args);
+           AiMod(&args);
        },
+       ParseModes::InfoMode => {
+           Infor();
+
+       }
    }
 }
 
